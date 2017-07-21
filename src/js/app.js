07e8d75ccfc9d1e4ae789122103a3447f5940e63
Muhammed Athimannil <hello@athimannil.com>
2017-07-21 13:47:11 +0200

    offer component with style

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { Header } from './components/Header';
+import { Offers } from './components/Offers';
 import { Footer } from './components/Footer';
 
 class App extends React.Component {
@@ -11,7 +12,7 @@ class App extends React.Component {
 		return(
 			<div className="app-wrap">
 				<Header/>
-				<main>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</main>
+				<Offers/>
 				<Footer/>
 			</div>
 		);
---
+++ src/js/components/Offers.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export class Offers extends React.Component {
+	render () {
+		return(
+			<main>
+				<div className="offer">
+					<h2>Title</h2>
+					<p>Location Name</p>
+				</div>
+				<div className="offer">
+					<h2>Title</h2>
+					<p>Location Name</p>
+				</div>
+				<div className="offer">
+					<h2>Title</h2>
+					<p>Location Name</p>
+				</div>
+				<div className="offer">
+					<h2>Title</h2>
+					<p>Location Name</p>
+				</div>
+			</main>
+		);
+	}
+}
